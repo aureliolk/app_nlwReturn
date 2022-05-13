e8a35c3fb921bf3c79ad7031075c8f1b5faa1325
Aurélio Castro <aurelio.cos@outlook.com>
2022-05-13 12:13:59 -0300

    Fix Limit Server

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3300;
 
 app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb' }));
+// app.use(express.urlencoded({ limit: '50mb' }));
 
 app.use(cors());
 app.use(router)
